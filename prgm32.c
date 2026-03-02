8bb9ddafea078e19e36ec9af862d72b258f5cffe
Vilas B K <vilasbk28@gmail.com>
2026-03-02 22:36:12 +0530

    calculator

---
+++ prgm32.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    char op;
+    printf("enter two numbers:");
+    scanf("%d%d",&a,&b);
+    printf("enter operator(+,-,*,/):");
+    scanf("%d%d",&a,&b);
+    printf("enter operator(+,-,*,/):");
+    scanf("%c",&op);
+    switch(op){
+        case'+':printf("rwsult=%d",a+b);break;
+        case'-':printf("result=%d",a-b);break;
+        case'*':printf("result=%d",a-b);break;
+        case'/':if(b!=0)
+        printf("result=%d",a/b);
+        else
+        printf("division by zero not allowed");
+        break;
+        default:printf("invalid operator");
+    }
+    return 0;
+}
+
